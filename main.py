3457258bf103dca8b4bc73eb32da1b19d927ced8
Lucas Inacio <lucas.in@outlook.com>
2025-02-03 13:38:18 -0300

    testando novas acoes

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import os
 def main():
     # Receber o valor passado pela GitHub Action
     nome = os.getenv('NOME')
-    return f'{nome}'
+    sobrenome = os.getenv('SOBRENOME')
+    cargo  = os.getenv('SOBRENOME')
+    return f'Nome: {nome}, Sobrenome: {sobrenome}, cargo: {cargo}'
 
 if __name__ == "__main__":
     main()
