52ad302b108d9e5e66c1b4765b9769068fea147f
Lucas Inacio <lucas.in@outlook.com>
2025-02-03 13:49:33 -0300

    printando dados do usuario

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     # Receber o valor passado pela GitHub Action
     nome = os.getenv('NOME')
     sobrenome = os.getenv('SOBRENOME')
-    cargo  = os.getenv('SOBRENOME')
+    cargo  = os.getenv('CARGO')
     return print(f'Nome: {nome}, Sobrenome: {sobrenome}, cargo: {cargo}')
 
 if __name__ == "__main__":
