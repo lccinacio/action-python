69457c48a924f657a93e7c1de1c994fe29dec83d
Lucas Inacio <lucas.in@outlook.com>
2025-02-03 13:08:54 -0300

    testando nome

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sys
+import os
 
 def main():
     # Receber o valor passado pela GitHub Action
     entrada = sys.argv[1]
     print(f'Recebi a entrada: {entrada}')
+    nome = os.getenv('NOME')
+    print(f'O nome dele é {nome}')
 
 if __name__ == "__main__":
     main()
